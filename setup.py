26a1fe08a011a549aa854e06e3a617c94b1bbd30
Stephen DeWitt <dewittsj@ornl.gov>
2024-08-19 11:15:18 -0400

    Add authors to setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     version='0.1.0',    
     description='A Python package for storing, sharing, and using information about materials in models and simulations.',
     url='https://code.ornl.gov/mist/mist',
-    author='Stephen DeWitt',
+    author='Stephen DeWitt, Gerry Knapp, Hope Spuck, Sam Reeve',
     author_email='dewittsj@ornl.gov',
     license='BSD 3-Clause',
     packages=['mistlib'],
