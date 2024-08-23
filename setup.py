9e60e3cde6a44b18fe4d1edc8c567e9d4ceb1f43
Sam Reeve <6740307+streeve@users.noreply.github.com>
2024-08-23 14:30:01 -0400

    Add black formatting; also add to pre-commit
    Co-authored-by: Stephen DeWitt <dewittsj@ornl.gov>

--- setup.py
+++ setup.py
@@ -8,19 +8,19 @@ with open(requirement_path) as f:
     requires = f.read().splitlines()
 
 setup(
-    name='mistlib',
-    version='0.1.0',
-    description='A Python package for storing, sharing, and using information about materials in models and simulations.',
-    url='https://code.ornl.gov/mist/mist',
-    author='Stephen DeWitt, Gerry Knapp, Hope Spuck, Sam Reeve',
-    author_email='dewittsj@ornl.gov',
-    license='BSD 3-Clause',
-    packages=['mistlib'],
+    name="mistlib",
+    version="0.1.0",
+    description="A Python package for storing, sharing, and using information about materials in models and simulations.",
+    url="https://code.ornl.gov/mist/mist",
+    author="Stephen DeWitt, Gerry Knapp, Hope Spuck, Sam Reeve",
+    author_email="dewittsj@ornl.gov",
+    license="BSD 3-Clause",
+    packages=["mistlib"],
     install_requires=requires,
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: BSD License'
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
     ],
 )
